df8b6d200cb5c36ced7965f70934d7fb3ed98e32
Kasper Falk <kafa21@student.bth.se>
2022-09-03 19:17:49 +0200

    Updated rule menu to display the settings

--- main.py
+++ main.py
@@ -2,23 +2,38 @@
 Main.py
 """
 from src.menu import menustart, menufile, menurule
+from src.input import Input
+from src.rules import Rules
 
 def main():
     """
     Main
     """
+    input_class = Input()
+    rules_class = Rules()
     while True:
         menustart()
 
         choice = input(" Enter a action --> ")
 
         if choice == "1":
+            """
+            Change file
+            """
             while True:
                 menufile()
                 choiceFile = input(" Enter a action --> ")
                 try:
                     if choiceFile == "q":
                         break
+                    elif choiceFile == "1":
+                        break # Maybe implement this | Sooner
+                    elif choiceFile == "2":
+                        new_file = input("Enter the file --> ")
+                        input_class.load_file(new_file)
+                        input_class.open_file()
+                        input(" Enter a action --> ")
+
                     else:
                         raise KeyError
                 except KeyError:
@@ -26,12 +41,19 @@ def main():
                     input("\nPress enter to go back to rule menu...")
 
         elif choice == "2":
+            """
+            Edit rules
+            """
             while True:
-                menurule()
-                choiceFile = input(" Enter a action --> ")
+                data = rules_class.read_json()
+                menurule(data)
+                choiceRule = input(" Enter a action --> ")
                 try:
-                    if choiceFile == "q":
+                    if choiceRule == "q":
                         break
+                    elif choiceRule == "1":
+                        rules_class.read_json()
+                        change_rule = input("Input which rule u want to change --> ")
                     else:
                         raise KeyError
                 except KeyError:
@@ -39,6 +61,9 @@ def main():
                     input("\nPress enter to go back to rule menu...")
 
         elif choice == "3":
+            """
+            Linter
+            """
             break
 
         elif choice == "q":
