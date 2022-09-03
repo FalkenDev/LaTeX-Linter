20f5be57f2a29ded58020485a918cc5bb834eb66
Kasper Falk <kafa21@student.bth.se>
2022-09-03 19:19:36 +0200

    Edited menurule function to display rule settings

--- src/menu.py
+++ src/menu.py
@@ -6,10 +6,11 @@ import fnmatch
 import os
 
 cleen_screen = chr(27) + "[2J" + chr(27) + "[;H"
-low_dash = "_" * 30 + "\n"
+low_dash = "_" * 40 + "\n"
 headerslash = "/" * 56
 green = '\x1b[1;32m'
 blue = '\x1b[1;34m'
+red = '\x1b[1;31m'
 purple = '\x1b[1;35m'
 end_color = '\x1b[0m'
 
@@ -33,6 +34,7 @@ def menustart():
     print(" 2 ) Edit rules")
     print(" 3 ) Start Lint the file / files")
     print(" q ) Exit / Close the program")
+    print(low_dash + "\n")
 
 def menufile():
     """Prints the Change File menu"""
@@ -50,14 +52,19 @@ def menufile():
     for file in os.listdir('./input'):
                 if fnmatch.fnmatch(file, '*.tex'):
                     print(" - " + file)
+                elif fnmatch.fnmatch(file, '*.bib'):
+                    print(" - " + file)
+                elif fnmatch.fnmatch(file, '*.tikz'):
+                    print(" - " + file)
     print(low_dash)
 
     print('{:^40s}'.format(purple + " Actions\n" + end_color))
     print(" 1 ) All Files")
     print(" 2 ) Specific File")
     print(" q ) Go Back")
+    print(low_dash + "\n")
 
-def menurule():
+def menurule(data):
     """Prints the Edit Rules menu"""
     print(cleen_screen)
 
@@ -69,8 +76,23 @@ def menurule():
     print(blue + "Rules:" + end_color + " Standard") # Edit the "standard" string to a function or a variable.
     print(low_dash)
 
-    print('{:^40s}'.format(blue + " Actions\n" + end_color))
-    print(" 1 ) Edit / Customize rules")
+    print(low_dash)
+    print('{:^49s}'.format(green + " Standard Rules " + end_color))
+    print('{:^53s}'.format(red + "( Unable to change Standard Rules ) \n" + end_color))
+    for i in data["standard"]:
+        print(blue + " " + i + ": "+ end_color + str(data["standard"][i]))
+    print(low_dash)
+
+    print(low_dash)
+    print('{:^49s}'.format(green + " User Rules\n" + end_color))
+    for i in data["user"]:
+        print(blue + " " + i + ": "+ end_color + str(data["user"][i]))
+    print(low_dash)
+
+    print('{:^49s}'.format(blue + " Actions\n" + end_color))
+    print(" 1 ) Edit User Rules")
     print(" 2 ) Change to Customized rules")
     print(" 3 ) Change to Standard rules")
     print(" q ) Go Back")
+    print(low_dash + "\n")
+
