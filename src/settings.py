64f1a2dd52df5f8419f63878a30d6bf17cee10cd
Kasper Falk <kafa21@student.bth.se>
2022-09-13 15:47:08 +0200

    Added functions for edit enviroment blocks

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,10 @@
 """ Settings Module """
 import json
 import ast
+from src.errors import AlredyExists, DontExists
+
+green = '\x1b[1;32m'
+end_color = '\x1b[0m'
 
 class Settings():
 
@@ -50,3 +54,41 @@ class Settings():
         json.dump(self.json_data, jsonfile)
         jsonfile.close()
         self.json_data = self.__read_json() 
+        print(green + " Setting rule: " + str(rule) + " has updated to have value: " + str(custom_input) + end_color)
+        return
+
+    def edit_enviroment_blocks_exclude_add(self, custom_input):
+        """
+        Adds user input to list in enviroment_blocks_exclude in json file
+        Raise exception AlredyExists if user input is alredy in the exclude list
+        """
+        jsonfile = open("./settings/settings.json", "w")
+
+        for block in self.json_data["customized"]["environment_blocks_exclude"]:
+            if block == custom_input:
+                raise AlredyExists
+        self.json_data["customized"]["environment_blocks_exclude"].append(custom_input)
+        json.dump(self.json_data, jsonfile)
+        jsonfile.close()
+        self.json_data = self.__read_json() 
+        print(green + " Setting rule: environment_blocks_exclude has added: " + str(custom_input) + " to the list." + end_color)
+        input("\n Press enter to go back to Edit Enviroment Blocks.")
+        return
+
+    def edit_enviroment_blocks_exclude_remove(self, custom_input):
+        """
+        Remove user input from the list in enviroment_blocks_exclude in json file
+        Raise exception DontExists if not user input is in the exclude list
+        """
+        jsonfile = open("./settings/settings.json", "w")
+
+        for block in self.json_data["customized"]["environment_blocks_exclude"]:
+            if str(block) == str(custom_input):
+                self.json_data["customized"]["environment_blocks_exclude"].remove(custom_input)
+                json.dump(self.json_data, jsonfile)
+                jsonfile.close()
+                self.json_data = self.__read_json() 
+                print(green + " Setting rule: environment_blocks_exclude has removed: " + str(custom_input) + " from the list." + end_color)
+                input("\n Press enter to go back to Edit Enviroment Blocks.")
+                return
+        raise DontExists
