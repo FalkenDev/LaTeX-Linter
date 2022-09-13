88f3dfacdb74732c830e7076ef807eae574a0bc0
Kasper Falk <kafa21@student.bth.se>
2022-09-13 13:03:37 +0200

    Added some reusable functions for the menu

--- src/menu.py
+++ src/menu.py
@@ -14,20 +14,34 @@ red = '\x1b[1;31m'
 purple = '\x1b[1;35m'
 end_color = '\x1b[0m'
 
+def menu_header(header_title, color):
+    "Prints menu header"
+    print(cleen_screen)
+    print(color + headerslash + end_color)
+    print("\n " + header_title + "\n")
+    print(color + headerslash + end_color)
+
+def menu_file_info(color, current_file):
+    "Prints current file info"
+    print(low_dash)
+    print(color + " Choosen File: " + end_color + current_file)
+    print(low_dash)
+
+def menu_settings_info(color, current_settings):
+    "Prints current settings"
+    print(low_dash)
+    print(color + " Settings: " + end_color + current_settings)
+    print(low_dash)
+
 
 def menu_start(current_settings, current_file):
     """Prints the start menu"""
-    print(cleen_screen)
 
-    print(green + headerslash + end_color)
-    print("\n Welcome to FalkenDev LaTeX Linter\n")
-    print(green + headerslash + end_color)
+    menu_header("Welcome to FalkenDev LaTeX Linter", green)
 
-    print(low_dash)
-    print(green + " Choosen File: " + end_color + current_file) # Edit the "Undefined" string to a function or a variable.
-    print("")
-    print(green + " Settings: " + end_color + current_settings) # Edit the "Standard" string to a function or a variable.
-    print(low_dash)
+    menu_file_info(green, current_file)
+
+    menu_settings_info(green, current_settings)
 
     print('{:^40s}'.format(green + " Actions\n" + end_color))
     print(" 1 ) Change File")
@@ -38,43 +52,28 @@ def menu_start(current_settings, current_file):
 
 def menu_file(current_file):
     """Prints the Change File menu"""
-    print(cleen_screen)
 
-    print(purple + headerslash + end_color)
-    print("\n Change File / Files\n")
-    print(purple + headerslash + end_color)
+    menu_header("Change File / Files", purple)
 
-    print(low_dash)
-    print(purple + "Choosen File: " + end_color + current_file) # Edit the "Undefined" string to a function or a variable.
-    print(low_dash)
+    menu_file_info(purple, current_file)
 
     print(purple + "Files to choose from input foler:\n" + end_color)
     for file in os.listdir('./input'):
-                if fnmatch.fnmatch(file, '*.tex'):
-                    print(" - " + file)
-                elif fnmatch.fnmatch(file, '*.bib'):
-                    print(" - " + file)
-                elif fnmatch.fnmatch(file, '*.tikz'):
-                    print(" - " + file)
+        if fnmatch.fnmatch(file, '*.tex'):
+            print(" - " + file)
     print(low_dash)
 
     print('{:^40s}'.format(purple + " Actions\n" + end_color))
-    print(" 1 ) All Files")
-    print(" 2 ) Specific File")
+    print(" 1 ) Specific File")
     print(" q ) Go Back")
     print(low_dash + "\n")
 
 def menu_settings(current_settings, standard_settings, customized_settings):
     """Prints the Edit Settings menu"""
-    print(cleen_screen)
 
-    print(blue + headerslash + end_color)
-    print("\n Settings\n")
-    print(blue + headerslash + end_color)
+    menu_header("Settings", blue)
 
-    print(low_dash)
-    print(blue + "Settings: " + end_color + current_settings) # Edit the "standard" string to a function or a variable.
-    print(low_dash)
+    menu_settings_info(blue, current_settings)
 
     print(low_dash)
     print('{:^49s}'.format(green + " Standard Settings " + end_color))
@@ -98,11 +97,8 @@ def menu_settings(current_settings, standard_settings, customized_settings):
 
 def menu_customize_settings(customized_settings):
     """ Prints the cusomized settings and gives options what to change """
-    print(cleen_screen)
 
-    print(blue + headerslash + end_color)
-    print("\n Edit Customized settings\n")
-    print(blue + headerslash + end_color)
+    menu_header("Edit Customized settings", blue)
 
     print(low_dash)
     print('{:^49s}'.format(green + " Customized Settings\n" + end_color))
