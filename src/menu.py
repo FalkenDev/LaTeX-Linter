5c82531f543a09e5d4d91ccad2f46f68c845641a
Kasper Falk <kafa21@student.bth.se>
2022-09-13 15:46:37 +0200

    Moved some functions from main to menu and added som functions

--- src/menu.py
+++ src/menu.py
@@ -4,6 +4,7 @@ menu.py
 
 import fnmatch
 import os
+from src.errors import WrongFile, WrongCommand, AlredyExists, DontExists
 
 cleen_screen = chr(27) + "[2J" + chr(27) + "[;H"
 low_dash = "_" * 40 + "\n"
@@ -57,7 +58,7 @@ def menu_file(current_file):
 
     menu_file_info(purple, current_file)
 
-    print(purple + "Files to choose from input foler:\n" + end_color)
+    print(purple + " Files to choose from input foler:\n" + end_color)
     for file in os.listdir('./input'):
         if fnmatch.fnmatch(file, '*.tex'):
             print(" - " + file)
@@ -110,5 +111,142 @@ def menu_customize_settings(customized_settings):
     print(" 1 ) Edit sentence-newline")
     print(" 2 ) Edit comment-space")
     print(" 3 ) Edit emptylines")
+    print(" 4 ) Edit Enviroment blocks")
     print(" q ) Go Back")
     print(low_dash + "\n")
+
+
+def change_file_function(ic):
+    """
+    Change file menu
+    """
+    while True:
+        menu_file(ic.get_current_file())
+        choiceFile = input(" Enter a action --> ")
+        try:
+            if choiceFile == "q":
+                """
+                Go back
+                """
+                break
+
+            elif choiceFile == "1":
+                """
+                Enters a specific file
+                """
+                new_file = input(" Enter the file --> ")
+                ic.set_file(new_file)
+                print(green + "\n File has updated to use: " + new_file + end_color)
+                input("\n Press enter to go back to main menu...")
+                break
+
+            raise WrongCommand # If wrong command has entered by user
+
+        except WrongCommand:
+            print("\n That is not a valid choice.")
+            input("\n Press enter to go back to rule menu...")
+
+        except WrongFile:
+            print("\n You can't use that file, please try again.")
+            input("\n Press enter to go back to rule menu...")
+
+def edit_sentence_newline(settings):
+    while True:
+        print(blue + "\n Editing sentence-newline\n" + end_color)
+
+        print(" 1 ) True")
+        print(" 2 ) False\n")
+        print(" q ) Go Back\n")
+
+        custom_input = input(" Enter a action --> ")
+
+        if custom_input == "1":
+            settings.edit_custom_settings("sentence-newline", True)
+            input("\n Press enter to go back to Settings Menu...")
+            break
+
+        elif custom_input == "2":
+            settings.edit_custom_settings("sentence-newline", False)
+            input("\n Press enter to go back to Settings Menu...")
+            break
+        
+        elif custom_input == "q":
+            break
+
+        print(red + "\n That is not a valid choice." + end_color)
+
+def edit_comment_space(settings):
+    while True:
+        menu_header("Editing comment-space", blue)
+        print("\n Enter value of how much you want the comment-space to be")
+        print("\n q ) Go back\n")
+
+        custom_input = input(" Enter a value --> ")
+
+        if custom_input.isnumeric():
+            settings.edit_custom_settings("comment-space", int(custom_input))
+            input("\n Press enter to go back to Settings Menu...")
+            break
+        
+        elif custom_input == "q":
+            break
+        
+        print(red + "\n That is not a valid choice." + end_color)
+
+def edit_emptylines(settings):
+    while True:
+        menu_header("Editing emptylines", blue)
+
+        custom_input = input(" Enter a value --> ")
+
+        if custom_input.isnumeric():
+            settings.edit_custom_settings("emptylines", int(custom_input))
+            input("\n Press enter to go back to Edit Customized settings menu...")
+            break
+
+        elif custom_input == "q":
+            break
+
+        print(red + "\n That is not a valid choice." + end_color)
+
+def edit_enviroment_blocks_exclude(settings, customized_settings):
+    while True:
+        menu_header("Edit Enviroment Blocks", blue)
+
+        print(low_dash)
+        print(" Enviroment blocks exclude list\n")
+        for block in customized_settings["environment_blocks_exclude"]:
+            print(" - " + block)
+        print(low_dash)
+
+        print(" Here you can choose if you want to exclude a enviroment block from /begin[itemize] or remove it from the exclude list\n")
+        print(" 1 ) Add a new Enviroment block to exclude")
+        print(" 2 ) Remove a Enviroment block from exclude list")
+        print(" q ) Go Back\n")
+
+        custom_input = input(" Enter a command --> ")
+
+        try:
+            if custom_input == "1":
+                user_input = input("\n Enter a name you want to exclude --> ")
+                settings.edit_enviroment_blocks_exclude_add(user_input)
+                customized_settings = settings.get_settings("customized")
+
+            elif custom_input == "2":
+                user_input = input("\n Enter a nem you want to remove from the exclude list --> ")
+                settings.edit_enviroment_blocks_exclude_remove(user_input)
+                customized_settings = settings.get_settings("customized")
+
+            elif custom_input == "q":
+                break
+
+            print(red + "\n That is not a valid choice." + end_color)
+
+        except AlredyExists:
+            print(red + "\n The name alredy exists in Enviroment blocks exclude list" + end_color)
+            input("\n Press enter to go back to Edit Enviroment Blocks")
+
+        except DontExists:
+            print(red + "\n The name dosen't exists in Enviroment blocks exclude list" + end_color)
+            input("\n Press enter to go back to Edit Enviroment Blocks")
+
