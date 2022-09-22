8c6dcac6aa6e192a38309b998e9d4e472003f481
Kasper Falk <kafa21@student.bth.se>
2022-09-22 18:27:34 +0200

    Implemented function rule_environment_blocks_exclude | Almost done

--- src/rules.py
+++ src/rules.py
@@ -1,6 +1,7 @@
 """ Rules Module """
 
 from ast import While
+from itertools import count
 from unittest import skip
 from src.errors import ErrorDataLoaded
 import shutil
@@ -19,8 +20,8 @@ class Rules():
             raise ErrorDataLoaded
 
         self.backup_file()
-        self.rule_emptylines()
-        #self.rule_environment_blocks_exclude()
+        self.rule_emptylines() # Done
+        self.rule_environment_blocks_exclude() # Almost Done - need to fix if it has to many \t ( Tabs ) not it just checks if it has \t
 
     def get_specific_settings(self, rule):
         """ Returns the specific setting rule """
@@ -53,8 +54,6 @@ class Rules():
         ]
 
         line_number_list = []
-        line_number_list_remove = []
-
         with open(self.file_path, "r") as files:
             for line_number, line in enumerate(files, start=1):
                 for word in rule_include:
@@ -130,26 +129,57 @@ class Rules():
     def rule_environment_blocks_exclude(self):
         """ Rule environment blocks exclude """
 
-        setting_list = self.get_specific_settings("environment_blocks_exclude")
+        setting_list = self.get_specific_settings("environment_blocks_exclude") # Get settings
 
-        word = r'''\begin{'''
+        word = r"\begin{" # Word to look after
 
-        test_list = []
+        setting_exclude_string_list = []
+        tab_index_list = []
+        begin_counter = 0
+        line_counter = 0
+        counter = 0
 
         for setting in setting_list:
-            test_list.append(r'''\begin{''' + setting + "}")
-
-        print(test_list)
-
-        end_value = 0
+            setting_exclude_string_list.append(word + setting + "}")
 
         with open(self.file_path, "r") as FILE:
             for line_number, line in enumerate(FILE, start=1):
-                if word in line:
+                if line_number <= counter - 1: # If the program alredy have checked those index position
+                    pass # Pass, Just get to next line_number and line
+                elif word in line.rstrip() and line.rstrip() not in setting_exclude_string_list: #If word is in the line and line is not on exclude list
+                    line_counter = 0 # Counter to get to next index line
+                    begin_counter = 1 # When get to a \begin{}
                     file = open(self.file_path)
                     content = file.readlines()
-                    if not content[line_number].startswith("\t"):
-                        print("Line: " + content[line_number - 1])
-                        print(setting)
-                        print(content[line_number])
-                        print(line_number)
+                    while True: # Checks trough the whole \begin to the begins \end
+                        if begin_counter == 0: # If the line in index have got to the last \end for the \begin
+                            break
+                        elif word in content[line_number + line_counter].strip(): # If it's a \begin in a \begin
+                            begin_counter += 1
+                            if not content[line_number + line_counter].startswith((begin_counter - 1) * "\t"):
+                                tab_index_list.append([(line_number + line_counter), ((begin_counter - 1) * "\t"), (content[line_number + line_counter].strip())])
+                            line_counter += 1
+                        elif "\\end" in content[line_number + line_counter].strip(): # If it's a \end in a \begin
+                            begin_counter -= 1
+                            if not content[line_number + line_counter].startswith((begin_counter - 1) * "\t"): # Why begin_counter - 1 is bcs the \begin should not have tab like the code after 
+                                tab_index_list.append([(line_number + line_counter), ((begin_counter - 1) * "\t"), (content[line_number + line_counter].strip())])
+                            line_counter += 1
+                        elif not content[line_number + line_counter].startswith(begin_counter * "\t"): # If it dosen't have correct \t ( Tab space )
+                            tab_index_list.append([(line_number + line_counter), (begin_counter * "\t"), (content[line_number + line_counter].strip())])
+                            line_counter += 1
+                        elif content[line_number + line_counter].startswith(begin_counter * "\t"): # If it has have correct \t ( Tab space ) then apply next line
+                            line_counter += 1
+
+                    counter = line_number + line_counter # How many index position the program have checked after the while loop
+
+        append_line = 0
+        l1 = []
+        with open(self.file_path, 'r') as fp:
+            l1 = fp.readlines()
+
+        for line in tab_index_list:
+            l1[line[0]] = line[1] + line[2] + "\n" #Replace the index position with how many tabs, the line and a \n
+
+        with open(self.file_path, "w") as file:
+            l1 = "".join(l1)
+            file.write(l1)
