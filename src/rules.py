b95349443e1510018f05acb8b452d17c69777ab8
Kasper Falk <kafa21@student.bth.se>
2022-09-22 14:49:20 +0200

    Rule emptylines finnished | Fixed bug with remove to many blank lines

--- src/rules.py
+++ src/rules.py
@@ -1,5 +1,7 @@
 """ Rules Module """
 
+from ast import While
+from unittest import skip
 from src.errors import ErrorDataLoaded
 import shutil
 import re
@@ -17,8 +19,8 @@ class Rules():
             raise ErrorDataLoaded
 
         self.backup_file()
-        #self.rule_emptylines()
-        self.rule_environment_blocks_exclude()
+        self.rule_emptylines()
+        #self.rule_environment_blocks_exclude()
 
     def get_specific_settings(self, rule):
         """ Returns the specific setting rule """
@@ -51,6 +53,7 @@ class Rules():
         ]
 
         line_number_list = []
+        line_number_list_remove = []
 
         with open(self.file_path, "r") as files:
             for line_number, line in enumerate(files, start=1):
@@ -58,13 +61,22 @@ class Rules():
                     if word in line:
                         file = open(self.file_path)
                         content = file.readlines()
-                        for i in range(1, setting_value):
-                            if content[line_number + i] != "\n":
-                                line_number_list.append(line_number + 1)
-                                for i in range(i + 1, setting_value):
-                                    line_number_list.append(line_number + 1)
-                                break
+                        mutable_setting_value = setting_value                                       # Reset the mutable to setting value
+                        if content[line_number ] != "\n":                                           # If has not "\n" before then apply all \n from settings value
+                            for i in range(0, mutable_setting_value):
+                                line_number_list.append(line_number)
+                        else:                                                                       # If alredy have "\n" before then count how many "\n" it has.
+                            counter = 0
+                            while True:
+                                if content[line_number + counter] == "\n":
+                                    mutable_setting_value = mutable_setting_value - 1
+                                    counter = counter + 1
+                                else:
+                                    break                                                           # When no more "\n" is in the line then break it
+                            for y in range(0, mutable_setting_value):                               # Apply the rest blank lines
+                                line_number_list.append(line_number)
                         file.close()
+
         append_line = 0
         l1 = []
         with open(self.file_path, 'r') as fp:
@@ -72,12 +84,48 @@ class Rules():
 
         for line in line_number_list:
             l1.insert(line + append_line, "\n")
-            append_line = append_line + 1
+            append_line = append_line + 1                                                           # Needs a append line bcs after every "\n" appended then it creates 1 more extra line
 
 
         with open(self.file_path, "w") as file:
             l1 = "".join(l1)
             file.write(l1)
+
+        self.replace_line_function(rule_include, setting_value)
+
+    def replace_line_function(self, rule_include, setting_value):
+        """ Find the index number and ignores it """
+        line_number_list_remove = []
+        with open(self.file_path, "r") as files:
+            for line_number, line in enumerate(files, start=1):
+                for word in rule_include:
+                    if word in line:
+                        file = open(self.file_path)
+                        content = file.readlines()
+                        mutable_setting_value = setting_value                                   # Reset the mutable to setting value
+                        counter = 0
+                        while True:
+                            if content[line_number + counter] == "\n":
+                                mutable_setting_value = mutable_setting_value - 1
+                                counter = counter + 1
+                                if counter > setting_value:                                     # If has more lines then setting_value
+                                    line_number_list_remove.append(line_number + counter)       # Append the line number
+                            else:
+                                break                                                           # When no more "\n" is in the line then break it
+                        file.close()
+
+        text = []
+        with open(self.file_path, "r") as files:
+            for line_number, line in enumerate(files, start=1):
+                if not line_number in line_number_list_remove:
+                     text.append(line)
+                else:
+                    continue                                                                    # If the line number is in the remove list, it just continues and not append the text line.
+
+        with open(self.file_path, "w") as file:
+            text = "".join(text)
+            file.write(text)
+
 
     def rule_environment_blocks_exclude(self):
         """ Rule environment blocks exclude """
