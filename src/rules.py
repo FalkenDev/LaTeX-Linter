e596fbb0c83000ebecfb41248666335fecad63a6
Kasper Falk <kafa21@student.bth.se>
2022-10-10 09:54:37 +0200

    Fixed bug with break sentence newline and fixed all opne files to with

--- src/rules.py
+++ src/rules.py
@@ -1,6 +1,8 @@
 """ Rules Module """
+# pylint: disable=line-too-long
 
 import shutil
+import re
 from sys import platform
 from src.errors import ErrorDataLoaded
 
@@ -20,13 +22,13 @@ class Rules():
         elif platform == "win32":
             self.newline = "\r\n"
 
-        # --- If soemthing has not passed in to the Rules class --- #
+        # --- If soemthing has not passed into the Rules class --- #
         if (self.json_settings_data is None or
             self.file_path == "undefined" or
             self.filename == "undefined"):
             raise ErrorDataLoaded
 
-        # --- Backup + the Linter rules --- #
+        # --- Running the Backup + the Linter rules --- #
         self.backup_file()
         self.rule_sentence_newline()
         self.rule_emptylines()
@@ -75,23 +77,22 @@ class Rules():
             for line_number, line in enumerate(file1, start=1):
                 for word in rule_include:
                     if word in line:
-                        file2 = open(self.file_path, encoding="utf-8")
-                        content = file2.readlines()
-                        mutable_setting_value = setting_value # Reset the mutable to setting value
-                        if content[line_number - 2] != newline: # If not \n before then apply all \n
-                            for _ in range(0, mutable_setting_value):
-                                line_number_list.append(line_number - 2)
-                        else: # If alredy have "\n" before then count how many "\n" it has.
-                            counter = 2 # Need to get to the upper index
-                            while True:
-                                if content[line_number - counter] == newline:
-                                    mutable_setting_value = mutable_setting_value - 1
-                                    counter = counter + 1
-                                else:
-                                    break # When no more "\n" is in the line then break it
-                            for _ in range(0, mutable_setting_value): # Apply the rest blank lines
-                                line_number_list.append(line_number - 2)
-                        file2.close()
+                        with open(self.file_path, "r", encoding="utf-8") as file2:
+                            content = file2.readlines()
+                            mutable_setting_value = setting_value # Reset the mutable to setting value
+                            if content[line_number - 2] != newline: # If not \n before then apply all \n
+                                for _ in range(0, mutable_setting_value):
+                                    line_number_list.append(line_number - 2)
+                            else: # If alredy have "\n" before then count how many "\n" it has.
+                                counter = 2 # Need to get to the upper index
+                                while True:
+                                    if content[line_number - counter] == newline:
+                                        mutable_setting_value = mutable_setting_value - 1
+                                        counter = counter + 1
+                                    else:
+                                        break # When no more "\n" is in the line then break it
+                                for _ in range(0, mutable_setting_value): # Apply the rest blank lines
+                                    line_number_list.append(line_number - 2)
 
         append_line = 0
         file_list = []
@@ -116,28 +117,26 @@ class Rules():
         newline = self.newline
         line_number_list_remove = []
 
-        with open(self.file_path, "r", encoding="utf-8") as files:
-            for line_number, line in enumerate(files, start=1):
+        with open(self.file_path, "r", encoding="utf-8") as file1:
+            for line_number, line in enumerate(file1, start=1):
                 for word in rule_include:
                     if word in line:
-                        file = open(self.file_path, encoding="utf-8")
-                        content = file.readlines()
-                        mutable_setting_value = setting_value
-                        counter = 0
-                        line_counter = 2
-                        while True:
-                            if content[line_number - line_counter] == newline:
-                                mutable_setting_value = mutable_setting_value - 1
-                                counter = counter + 1
-                                line_counter = line_counter + 1
-                                if counter > setting_value: # If more blanklines the settings value
-                                    line_number_list_remove.append( # Append the line number
-                                        line_number - line_counter
-                                    )
-                            else:
-                                break
-                        file.close()
-
+                        with open(self.file_path, "r", encoding="utf-8") as file2:
+                            content = file2.readlines()
+                            mutable_setting_value = setting_value
+                            counter = 0
+                            line_counter = 2
+                            while True:
+                                if content[line_number - line_counter] == newline:
+                                    mutable_setting_value = mutable_setting_value - 1
+                                    counter = counter + 1
+                                    line_counter = line_counter + 1
+                                    if counter > setting_value: # If more blanklines the settings value
+                                        line_number_list_remove.append( # Append the line number
+                                            line_number - line_counter
+                                        )
+                                else:
+                                    break
         text = []
         with open(self.file_path, "r", encoding="utf-8") as file:
             for line_number, line in enumerate(file, start=1):
@@ -168,47 +167,49 @@ class Rules():
         for setting in setting_list:
             setting_exclude_string_list.append(word + setting + "}")
 
+        print(setting_exclude_string_list)
+
         with open(self.file_path, "r", encoding="utf-8") as file1:
             for line_number, line in enumerate(file1, start=1):
                 if line_number <= counter - 1: # If the program alredy have checked those index position
                     pass
-                elif word in line.rstrip() and line.rstrip() not in setting_exclude_string_list and not line.startswith("%"): # If word is in the line and line is not on exclude list and it's not a comment block
+                elif word in line.rstrip() and any(exclude in line for exclude in setting_exclude_string_list) is False and not line.startswith("%"): # If word is in the line and line is not on exclude list and it's not a comment block
                     line_counter = 0 # Counter to get to next index line
                     begin_counter = 1 # When get to a \begin{}
-                    file2 = open(self.file_path, encoding="utf-8")
-                    content = file2.readlines()
-                    tab_index_list.append([
-                        (line_number + line_counter - 1),
-                        ((begin_counter - 1) * tab),
-                        (content[line_number + line_counter - 1].strip())
-                    ])
-                    while True: # Checks trough the whole \begin to the begins \end
-                        if begin_counter == 0: # If the line in index have got to the last \end for the \begin
-                            break
-                        if word in content[line_number + line_counter].strip():
-                            begin_counter += 1
-                            tab_index_list.append([
-                                (line_number + line_counter),
-                                ((begin_counter - 1) * tab),
-                                (content[line_number + line_counter].strip())
-                            ])
-                            line_counter += 1
-                        elif "\\end" in content[line_number + line_counter].strip():
-                            tab_index_list.append([
-                                (line_number + line_counter),
-                                ((begin_counter - 1) * tab),
-                                (content[line_number + line_counter].strip())
-                            ])
-                            begin_counter -= 1
-                            line_counter += 1
-                        else:
-                            tab_index_list.append([
-                                (line_number + line_counter),
-                                (begin_counter * tab),
-                                (content[line_number + line_counter].strip())])
-                            line_counter += 1
+                    with open(self.file_path, "r", encoding="utf-8") as file2:
+                        content = file2.readlines()
+                        tab_index_list.append([
+                            (line_number + line_counter - 1),
+                            ((begin_counter - 1) * tab),
+                            (content[line_number + line_counter - 1].strip())
+                        ])
+                        while True: # Checks trough the whole \begin to the begins \end
+                            if begin_counter == 0: # If the line in index have got to the last \end for the \begin
+                                break
+                            if word in content[line_number + line_counter].strip():
+                                begin_counter += 1
+                                tab_index_list.append([
+                                    (line_number + line_counter),
+                                    ((begin_counter - 1) * tab),
+                                    (content[line_number + line_counter].strip())
+                                ])
+                                line_counter += 1
+                            elif "\\end" in content[line_number + line_counter].strip():
+                                tab_index_list.append([
+                                    (line_number + line_counter),
+                                    ((begin_counter - 1) * tab),
+                                    (content[line_number + line_counter].strip())
+                                ])
+                                begin_counter -= 1
+                                line_counter += 1
+                            else:
+                                tab_index_list.append([
+                                    (line_number + line_counter),
+                                    (begin_counter * tab),
+                                    (content[line_number + line_counter].strip())])
+                                line_counter += 1
 
-                    counter = line_number + line_counter # How many index position the program have checked after the while loop
+                        counter = line_number + line_counter # How many index position the program have checked after the while loop
 
         file_list = []
         with open(self.file_path, 'r', encoding="utf-8") as file:
@@ -269,13 +270,13 @@ class Rules():
         if sentence_newline is True:
             with open(self.file_path, "r", encoding="utf-8") as file:
                 for line_number, line in enumerate(file, start=1):
-                    if ". " in line:
+                    if re.search("[?!.][ ]", line) and not line.startswith("%"):
                         same_line_number = 0
                         line_length = len(line)
                         for char_index, char in enumerate(line, start=1):
                             if line_length <= (char_index + 1):
                                 break
-                            if char == "." and line[char_index] is " ":
+                            if re.search("[?!.]", char) and line[char_index] is " ":
                                 if same_line_number is line_number:
                                     line1 = ""
                                 else:
